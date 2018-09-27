49e5e695e8539a08a074d6d9c8e1c667cdd0dd92
qiaohoucheng <qiaohoucheng@sina.com>
2018-09-27 18:27:50 +0800

    wap view and css

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,13 +4,47 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use App\Model\Theme;
+use App\Model\Option;
 
 class IndexController extends Controller
 {
-    //
-
-    public function index()
+    protected  $vid;
+    public function __construct()
     {
-        return view('welcome');
+        $this->vid = 1;
+    }
+    //首页
+    public function index(Request $request)
+    {
+        $keyword = $request->input('keyword') ? $request->input('keyword') :'';
+        $data = Theme::find($this->vid);
+        return view('index.index',compact('data','keyword'));
+    }
+    //加载数据
+    public function load_data()
+    {
+
+    }
+    //搜索 有可能会合并到index
+    public function search()
+    {
+
+    }
+    //详情
+    public function show($id)
+    {
+        $data = Option::find($id);
+        return view('index.detail',compact('id','data'));
+    }
+    //投票
+    public function store()
+    {
+
+    }
+    //提交审核
+    public function update()
+    {
+
     }
 }

1ec64be73b511141521e3acd131e13631164d48c
qiaohoucheng <qiaohoucheng@sina.com>
2018-09-28 15:55:58 +0800

    wexin 1.2

--- resources/views/index/detail.blade.php
+++ resources/views/index/detail.blade.php
@@ -383,7 +383,7 @@
         //提交上传图片
     })
 </script>
-<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>
 <script type="text/javascript">
     $(function(){
         var url = location.href.split('#').toString();
--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -279,7 +279,7 @@
         });
     });
 </script>
-<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>
 <script type="text/javascript">
     $(function(){
         var url = location.href.split('#').toString();
